3818911eabaa99a0f360aa1c085760d526b32911
Riley Campbell <116246538+RileyCampbell2007@users.noreply.github.com>
2025-04-08 17:33:16 -0600

    Disable DEVMODE

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import sam
 import hivex
 
 # Constants
-DEVMODE = True
+DEVMODE = False
 LICENSE = open(f'{os.path.dirname(os.path.realpath(__file__))}/LICENSE').read().replace('\n\n','Placeholder').replace('\n  ','').replace('\n',' ').replace('Placeholder','\n\n')
 MOUNT_POINT = Path("/mnt")
 WINDOWS_SAM_PATH = Path("Windows/System32/config/SAM")
